2bdb2002537d21110a4302768161c101b603d553
buivantai2303 <buivantai2303@gmail.com>
2024-05-31 14:22:10 +0700

    Setting up the utilities Create Files for constants Device Utilities Formatters (Date, Currency, Phone number) Helper functions HTTP client (for handling api calls) Local Storage Logger (warning message, error message etc) Validator
    Add Native Splash Package
    Create new file for Splash
    Add color and image to native splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
-import 'package:firebase_eco/util/theme/theme.dart';
+import 'package:firebase_eco/utils/theme/theme.dart';
 import 'package:flutter/material.dart';
 
 void main() {
+  // Todo: Add Widgets Binding
+  // Todo: Init Local Storage
+  // Todo: Await Native Splash
+  // Todo: Initialize Firebase
+  // Todo: Initialize Authentication
+
   runApp(const App());
 }
 
---
+++ lib/utils/formatters/formatter.dart
@@ -0,0 +1,25 @@
+import 'package:intl/intl.dart';
+
+class TFormatter {
+  static String formatDate(DateTime? date) {
+    date ??= DateTime.now();
+    return DateFormat('dd-MMM-yyyy').format(date);
+  }
+
+  static String formatCurrency(double amount) {
+    return NumberFormat.currency(locale: 'en US', symbol: '\$')
+        .format(amount); // Customize the currency locale and symbol as neec
+  }
+
+  static String formatPhoneNumber(String phoneNumber) {
+    // Assuming a 10-digit US phone number format: (123) 456-7898
+    if (phoneNumber.length == 10) {
+      return '(${phoneNumber.substring(0, 3)}) ${phoneNumber.substring(3, 6)} ${phoneNumber.substring(6)}';
+    } else if (phoneNumber.length == 11) {
+      return '(${phoneNumber.substring(0, 4)}) ${phoneNumber.substring(4, 7)} ${phoneNumber.substring(7)}';
+    }
+
+    // Add more custom phone number formatting logic for different formats if needed.
+    return phoneNumber;
+  }
+}
