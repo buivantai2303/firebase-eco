ecca2a1d127775482bd37e62169b7842501da73b
Quoc Viet <vietddls06@gmail.com>
2024-06-02 21:05:53 +0700

    vd8

--- lib/features/authentication/screens/signup/widget/signup.dart
+++ lib/features/authentication/screens/signup/widget/signup.dart
@@ -1,38 +1,52 @@
+import 'package:firebase_eco/utils/constants/sizes.dart';
+import 'package:firebase_eco/utils/constants/text_strings.dart';
+import 'package:firebase_eco/utils/constants/texts.dart';
 import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart'; 
 
-class SignupScreen extends StatelessWidget{
-  const SignupScreen({super.key})
+class SignupScreen extends StatelessWidget {
+  const SignupScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
       body: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.all(TSizes.defaultSpace),
-            child: Column(
-                children: [
-                ],
-                /// Title
-                Text(TTexts.signupTitle, style: Theme. of (context).text Theme.headlineMedium), const SizedBox(height: TSizes.spaceBtwSections),
-                /// Form
-                Form(
-                -child: Column(
-                children: [
-                ],
-                Row(
-                children: [
-                1,
-                TextFormField(
-            expands: false,
-            -decoration: const InputDecoration (labelText: TTexts.firstName, prefixIcon: Icon (Iconsax.user
-            ), // TextFormField
-          ) // Row
-      ), // Column
-    ), // Form
-    ), // Column
-    ), // Padding
-    ), // SingleChildScrollView
-    ),
+        child: Padding(
+          padding: const EdgeInsets.all(TSizes.defaultSpace),
+          child: Column(
+            children: [
+              /// Title
+              Text(
+                TTexts.signupTitle,
+                style: Theme.of(context).textTheme.headlineMedium,
+              ),
+              const SizedBox(height: TSizes.spaceBtwSections),
+
+              /// Form
+              Form(
+                child: Column(
+                  children: [
+                    Row(
+                      children: [
+                        Expanded(
+                          child: TextFormField(
+                            expands: false,
+                            decoration: const InputDecoration(
+                              labelText: TTexts.firstName,
+                              prefixIcon: Icon(Iconsax.user),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
+  }
 }
