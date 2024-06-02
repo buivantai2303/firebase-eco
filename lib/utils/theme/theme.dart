1c2d94b30c461caf5f195830374fb65aa45da5ea
buivantai2303 <buivantai2303@gmail.com>
2024-06-02 16:00:32 +0700

    Update path

--- lib/utils/theme/theme.dart
+++ lib/utils/theme/theme.dart
@@ -1,14 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_eco/utils/theme/widget_themes/appbar_theme.dart';
-import 'package:firebase_eco/utils/theme/widget_themes/bottom_sheet_theme.dart';
-import 'package:firebase_eco/utils/theme/widget_themes/checkbox_theme.dart';
-import 'package:firebase_eco/utils/theme/widget_themes/chip_theme.dart';
-import 'package:firebase_eco/utils/theme/widget_themes/elevated_button_theme.dart';
-import 'package:firebase_eco/utils/theme/widget_themes/outlined_button_theme.dart';
-import 'package:firebase_eco/utils/theme/widget_themes/text_field_theme.dart';
-import 'package:firebase_eco/utils/theme/widget_themes/text_theme.dart';
-
 import '../constants/colors.dart';
+import 'custom_themes/appbar_theme.dart';
+import 'custom_themes/bottom_sheet_theme.dart';
+import 'custom_themes/checkbox_theme.dart';
+import 'custom_themes/chip_theme.dart';
+import 'custom_themes/elevated_button_theme.dart';
+import 'custom_themes/outlined_button_theme.dart';
+import 'custom_themes/text_field_theme.dart';
+import 'custom_themes/text_theme.dart';
 
 class TAppTheme {
   TAppTheme._();
