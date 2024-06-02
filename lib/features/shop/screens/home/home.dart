2d2d6507e27b0d9b5f152feb36d67bb1ab8086b6
JAYCYISME\doquo <quoctuanisme0610@gmail.com>
2024-06-02 17:27:57 +0700

    categories

--- lib/features/shop/screens/home/home.dart
+++ lib/features/shop/screens/home/home.dart
@@ -1,5 +1,15 @@
+import 'package:firebase_eco/features/shop/screens/home/widgets/home_categories.dart';
+import 'package:firebase_eco/utils/constants/colors.dart';
+import 'package:firebase_eco/utils/constants/image_strings.dart';
+import 'package:firebase_eco/utils/constants/sizes.dart';
+import 'package:firebase_eco/utils/device/device_utility.dart';
+import 'package:firebase_eco/utils/helpers/helper_functions.dart';
 import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
 import '../../../../common/widgets/custom_shape/container/primary_header_container.dart';
+import '../../../../common/widgets/custom_shape/container/search_container.dart';
+import '../../../../common/widgets/image_text_widgets/vertical_image_text.dart';
+import '../../../../common/widgets/texts/action_heading.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -10,10 +20,44 @@ class HomeScreen extends StatelessWidget {
       body: SingleChildScrollView(
         child: Column(
           children: [
-            TPrimaryHeaderContainer(child: Container()),
+            TPrimaryHeaderContainer(
+                child: Container(
+              child: Column(
+                children: [
+                  /// App Bar
+                  THomeAppBar(),
+                  SizedBox(height: TSizes.spaceBtwSections),
+
+                  TSearchContainer(text: 'Search in Store'),
+                  SizedBox(height: TSizes.spaceBtwSections),
+
+                  Padding(
+                    padding: EdgeInsets.only(left: TSizes.defaultSpace),
+                    child: Column(
+                      children: [
+                        ///Heading
+                        TSectionHeading(
+                          title: 'Popular Categories',
+                          showActionButton: false,
+                          textColor: Colors.white,
+                        ),
+                        const SizedBox(
+                          height: TSizes.spaceBtwItems,
+                        ),
+
+                        ///Categories
+                        THomeCategories(),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            )),
           ],
         ),
       ),
     );
   }
 }
+
+
