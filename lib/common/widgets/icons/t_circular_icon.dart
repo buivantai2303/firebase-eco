1477993f42c65ccf088c0774e7ebe33addf95a5d
JAYCYISME\doquo <quoctuanisme0610@gmail.com>
2024-06-03 19:11:50 +0700

    Product Card Design

---
+++ lib/common/widgets/icons/t_circular_icon.dart
@@ -0,0 +1,43 @@
+import 'package:firebase_eco/utils/constants/colors.dart';
+import 'package:firebase_eco/utils/helpers/helper_functions.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_eco/utils/constants/sizes.dart';
+
+class TCircularIcon extends StatelessWidget {
+  const TCircularIcon({
+    Key? key, // Added Key parameter here
+    required this.icon,
+    this.width,
+    this.height,
+    this.size = TSizes.lg,
+    this.onPressed,
+    this.color,
+    this.backgroundColor,
+  }) : super(key: key); // Passed the key parameter to super constructor
+
+  final double? width, height, size;
+  final IconData icon;
+  final Color? color;
+  final Color? backgroundColor;
+  final VoidCallback? onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      borderRadius: BorderRadius.circular(100),
+      color: backgroundColor != null
+          ? backgroundColor!
+          : THelperFunctions.isDarkMode(context)
+          ? TColors.black.withOpacity(0.9)
+          : TColors.white.withOpacity(0.9),
+      child: IconButton(
+        onPressed: onPressed,
+        icon: Icon(
+          icon,
+          color: color,
+          size: size,
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/common/widgets/layouts/grid_layout.dart
@@ -0,0 +1,31 @@
+import 'package:firebase_eco/utils/constants/sizes.dart';
+import 'package:flutter/material.dart';
+
+class TGridLayout extends StatelessWidget {
+  const TGridLayout ({
+    super.key,
+    required this.itemCount,
+    required this.itemBuilder,
+    this.mainAxisExtent = 288,
+});
+  
+  final int itemCount;
+  final double? mainAxisExtent;
+  final Widget? Function(BuildContext, int) itemBuilder;
+  
+  @override
+  Widget build(BuildContext context) {
+    return GridView.builder(
+        itemCount: itemCount,
+        shrinkWrap: true,
+        padding: EdgeInsets.zero,
+        physics: const NeverScrollableScrollPhysics(),
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          mainAxisExtent: mainAxisExtent,
+          mainAxisSpacing: TSizes.gridViewSpacing,
+          crossAxisSpacing: TSizes.gridViewSpacing,
+        ),
+        itemBuilder: itemBuilder);
+  }
+}
