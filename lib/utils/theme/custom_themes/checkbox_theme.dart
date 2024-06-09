406ff20accb56ee4c1ec28d6bd1e42e768cb67dd
JonesKhaii <duongkhaipham@gmail.com>
2024-06-09 20:45:44 +0700

    Fetch category from data 70%

--- lib/utils/theme/custom_themes/checkbox_theme.dart
+++ lib/utils/theme/custom_themes/checkbox_theme.dart
@@ -9,15 +9,15 @@ class TCheckboxTheme {
   /// Customizable Light Text Theme
   static CheckboxThemeData lightCheckboxTheme = CheckboxThemeData(
     shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(TSizes.xs)),
-    checkColor: WidgetStateProperty.resolveWith((states) {
-      if (states.contains(WidgetState.selected)) {
+    checkColor: MaterialStateProperty.resolveWith((states) {
+      if (states.contains(MaterialState.selected)) {
         return TColors.white;
       } else {
         return TColors.black;
       }
     }),
-    fillColor: WidgetStateProperty.resolveWith((states) {
-      if (states.contains(WidgetState.selected)) {
+    fillColor: MaterialStateProperty.resolveWith((states) {
+      if (states.contains(MaterialState.selected)) {
         return TColors.primary;
       } else {
         return Colors.transparent;
@@ -28,15 +28,15 @@ class TCheckboxTheme {
   /// Customizable Dark Text Theme
   static CheckboxThemeData darkCheckboxTheme = CheckboxThemeData(
     shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(TSizes.xs)),
-    checkColor: WidgetStateProperty.resolveWith((states) {
-      if (states.contains(WidgetState.selected)) {
+    checkColor: MaterialStateProperty.resolveWith((states) {
+      if (states.contains(MaterialState.selected)) {
         return TColors.white;
       } else {
         return TColors.black;
       }
     }),
-    fillColor: WidgetStateProperty.resolveWith((states) {
-      if (states.contains(WidgetState.selected)) {
+    fillColor: MaterialStateProperty.resolveWith((states) {
+      if (states.contains(MaterialState.selected)) {
         return TColors.primary;
       } else {
         return Colors.transparent;
