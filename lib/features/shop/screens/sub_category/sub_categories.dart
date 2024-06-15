6bc4593f732ceb413feacf91ffda97dd28447f80
JAYCYISME\doquo <quoctuanisme0610@gmail.com>
2024-06-15 16:39:39 +0700

    Sub Category Screen

---
+++ lib/features/shop/screens/sub_category/sub_categories.dart
@@ -0,0 +1,48 @@
+import 'package:firebase_eco/common/widgets/appbar/appbar.dart';
+import 'package:firebase_eco/common/widgets/custom_shape/container/rounded_container.dart';
+import 'package:firebase_eco/common/widgets/images/t_rounded_image.dart';
+import 'package:firebase_eco/common/widgets/products/product_cards/product_card_horizontal.dart';
+import 'package:firebase_eco/common/widgets/texts/section_heading.dart';
+import 'package:firebase_eco/utils/constants/image_strings.dart';
+import 'package:firebase_eco/utils/constants/sizes.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+
+class SubCategoriesScreen extends StatelessWidget {
+  const SubCategoriesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: TAppbar(title: Text("Sports shirts"), showBackArrow: true,),
+      body: SingleChildScrollView(
+        child: Padding(
+            padding: const EdgeInsets.all(TSizes.defaultSpace),
+          child: Column(
+            children: [
+              TRoundedImage(imageUrl: TImages.promoBanner1, applyImageRadius: true, width: double.infinity),
+              SizedBox(width: TSizes.spaceBtwSections,),
+
+              Column(
+                children: [
+                  TSectionHeading(title: 'Sport shirts', onPressed: () {}),
+                  const SizedBox(height: TSizes.spaceBtwItems / 2,),
+
+                  SizedBox(
+                    height: 120,
+                    child: ListView.separated(
+                      itemCount: 4, scrollDirection: Axis.horizontal,
+                        separatorBuilder: (context, index) => const SizedBox(width: TSizes.spaceBtwItems,),
+                        itemBuilder: (context, index) => const TProductCardHorizontal()),
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
